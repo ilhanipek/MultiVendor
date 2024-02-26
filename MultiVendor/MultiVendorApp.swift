603b6039931630e8a0a8756e3e7a07422724e675
İlhan İpek <aliilhanipek92@gmail.com>
2024-02-26 13:36:40 +0300

    Initial Commit

---
+++ MultiVendor/MultiVendorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiVendorApp.swift
+//  MultiVendor
+//
+//  Created by ilhan serhan ipek on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiVendorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
